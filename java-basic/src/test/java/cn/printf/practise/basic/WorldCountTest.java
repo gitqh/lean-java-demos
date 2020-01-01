d1cfd2a31c87add42d1c03ff811c58268433b3d9
linksgo2011 <120377843@qq.com>
2020-01-01 20:32:25 +0800

    implement world count by map

---
+++ java-basic/src/test/java/cn/printf/practise/basic/WorldCountTest.java
@@ -0,0 +1,50 @@
+package cn.printf.practise.basic;
+
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * 使用 HashMap 实现单词统计
+ */
+public class WorldCountTest {
+    private String text = "you are not alone and you are a good man";
+
+    @Test
+    public void shouldOutWorldCunt() {
+        HashMap<String, Letter> stringLetterHashMap = new HashMap<String, Letter>();
+
+        String[] words = text.split(" ");
+        for (String world : words) {
+            if (!stringLetterHashMap.containsKey(world)) {
+                stringLetterHashMap.put(world,new Letter());
+            }
+
+            stringLetterHashMap.get(world).updateCount();
+        }
+
+        Iterator<Map.Entry<String, Letter>> iterator1 = stringLetterHashMap.entrySet().iterator();
+
+        while (iterator1.hasNext()){
+            Map.Entry<String, Letter> entry = iterator1.next();
+            String line = entry.getKey() + ":" + entry.getValue().getCount();
+            System.out.println(line);
+        }
+
+    }
+
+    class Letter {
+        private int count = 0;
+
+        public void updateCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return this.count;
+        }
+    }
+}
